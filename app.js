d69e4928ff56ff02e175c11da9c5aa9114c4b71c
ahmad <radio_frequency@hotmail.com>
2023-07-09 09:13:23 -0400

    update code to reflect more random messages

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 var ScheduleJobHandler = require('./core/schedulejobhandler');
 var WhatsAppJob = require('./core/jobs/whatsapp');
-
+var Config = require("./models/config")
 var handler = ScheduleJobHandler.getInstance();
 whatsAppJob = new WhatsAppJob([""]);
+var morning = Config.morningMessages;
+var happyEmojis = Config.happyEmojis;
+var night = Config.nightMessages;
 
-//0 5 * * *
-handler.addJob('whatsapp-morning', '*/1 * * * *', function(){
+handler.addJob('whatsapp-morning', '0 5 * * *', function(){
   console.log("running whatsapp morning message sender...")
   try {
-    whatsAppJob.sendMessageViaName("Good Morning");
+
+    const randomElement = morning[Math.floor(Math.random() * morning.length)];
+    const randomEmoji = happyEmojis[Math.floor(Math.random() * happyEmojis.length)];
+
+    whatsAppJob.sendMessageViaName(randomElement + " " + randomEmoji);
   } catch (error) {
     console.warn("error occured running whatsapp morning job:" + error);
   }
 });
 
-//0 22 * * *
-handler.addJob('whatsapp-evening', '*/1 * * * *', function(){
+handler.addJob('whatsapp-evening', '0 22 * * *', function(){
   console.log("running whatsapp morning message sender...")
   try {
-    whatsAppJob.sendMessageViaName("Have a good night");
+    const randomElement = night[Math.floor(Math.random() * night.length)];
+    const randomEmoji = happyEmojis[Math.floor(Math.random() * happyEmojis.length)];
+
+    whatsAppJob.sendMessageViaName(randomElement + " " + randomEmoji);
   } catch (error) {
     console.warn("error occured running whatsapp evening job:" + error);
   }
---
+++ models/config.js
@@ -0,0 +1,21 @@
+const morningMessages = [
+  "Good Morning", "Inshallah have a great day ahead", "I hope you slept well",
+  "Salam Walakum and enjoy your day ahead", "Inshallah have a blessed day ahead"
+];
+
+const nightMessages = [
+  "Good Evening", "Inshallah you sleep well", "I hope you will sleep well",
+  "Good night", "Sleep well", "Sweet dreams"
+];
+
+const happyEmojis = [
+  "💙",
+  "💕",
+  "🤸‍♂",
+  "🐰",
+  "👌",
+  "👋",
+  "😝"
+]
+
+module.exports = {morningMessages, nightMessages, happyEmojis}  
